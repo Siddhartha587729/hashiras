b4d773236ceab1677983dd14c81f2a79977735d3
Siddhartha587729 <Siddhartha587729@gmail.com>
2024-03-17 12:23:11 +0530

    [feature] ui added

---
+++ src/components/Display.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Display() {
+  return (
+    <div className="w-2/4 bg-[#000000] h-screen p-2">Display</div>
+  )
+}
+
+export default Display
---
+++ src/components/Remote.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Remote() {
+  return (
+    <div className='w-1/4 bg-[#0c0908de] h-screen p-2'>Remote</div>
+  )
+}
+
+export default Remote
