094470f24828e35554b8a9c384e7df59fc3843f3
Siddhartha587729 <Siddhartha587729@gmail.com>
2024-03-17 12:56:00 +0530

    [feature] buttons added

--- src/reducers/res.js
+++ src/reducers/res.js
@@ -1,6 +1,6 @@
-export default(state={des:[]},action)=>{
+/* export default(state={des:[]},action)=>{
     switch(action.type){
         case STATUS:
             return {...state,des:state.des}
     }
-}
+} */
