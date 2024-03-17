b7153108ed16a7f11114d0378cd7bc76a264ee57
Siddhartha587729 <Siddhartha587729@gmail.com>
2024-03-17 22:18:43 +0530

    images added

--- src/components/Remote.jsx
+++ src/components/Remote.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react'
 
 function Remote({device}) {
   const [data,setData]=useState('');
+  const [value,setValue]=useState(0);
   useEffect(()=>{
     setData(device);
+    setValue(device.value)
   },)
   const handleClick=()=>{
     data.value-=1;
@@ -13,17 +15,20 @@ function Remote({device}) {
   }
   console.log(data);
   return (
-    <div className='w-1/4 h-screen p-2'>
-      <div className='h-1/2  bg-[#0c0908de]'>
-      </div>
-      <div flex>
-        <div className='bg-[#0c0908de] rounded-md p-25'>
-        <button onClick={()=>handleClick()}></button>
+    <div className='px-5 py-10 flex flex-col items-center'>
+        <div>
+            <span className='text-3xl'>{device.device}</span>
         </div>
-        <div className='bg-[#0c0908de] rounded-md p-25'>
-          <button onClick={()=>increaseValue()}></button>
+        <div className='h-[250px] mt-3 overflow-hidden'>
+            <img src={data.icon} alt="" />
         </div>
-      </div>
+        <div className="mt-10 w-[200px] h-[200px] border-2 bg-blue-500 rounded-full flex items-center justify-center text-5xl">
+        {data.device !== 'AC' ? value : null}
+        </div>
+        <div className='flex gap-36'>
+            <div className="my-10 w-12 h-12 border-2 bg-white rounded-full flex items-center justify-center text-black font-semibold text-3xl hover:cursor-pointer">-</div>
+            <div className="my-10 w-12 h-12 border-2 bg-white rounded-full flex items-center justify-center text-black text-3xl font-semibold hover:cursor-pointer">+</div>
+        </div>
     </div>
   )
 }
--- src/utils/device.js
+++ src/utils/device.js
@@ -1,21 +1,34 @@
-import { getDataFromAPI } from "../api";
+/* import { getDataFromAPI } from "../api";
 const data=await getDataFromAPI();
+console.log(data) */
+
+import ac from "../assets/ac.png"
+import fan from "../assets/fan.png"
+import bulb from "../assets/bulb.png"
+import led from "../assets/led.png"
+import { Fan,Lightbulb,Siren,AirVent } from 'lucide-react';
 
 const devices = [
     {
       teamid: "8nCa1hq",
       device: "Fan",
-      value: 0
+      value: 0,
+      ic:Fan,
+      icon:fan
     },
     {
       teamid: "8nCa1hq",
       device: "Bulb",
-      value: 1
+      value: 1,
+      ic:Lightbulb,
+      icon: bulb
     },
     {
       teamid: "8nCa1hq",
       device: "LED",
-      value: "#000000"
+      value: "#000000",
+      ic: Siren,
+      icon: led
     },
     {
       teamid: "8nCa1hq",
@@ -23,7 +36,9 @@ const devices = [
       value:{
         "temp": 24,
         "state": 0
-      }
+      },
+      ic:AirVent,
+      icon: ac
     },
   ];
 
