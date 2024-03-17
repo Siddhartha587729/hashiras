4265f10490e51ab2fb1101df11b67231b4382a18
Sahil Kumar <sahilku601@gmail.com>
2024-03-17 13:10:26 +0530

    redux updates

--- src/reducers/res.js
+++ src/reducers/res.js
@@ -1,8 +1,9 @@
-/* export default(state={des:[]},action)=>{
+import {STATUS} from '../constants/actions';
+export default(state={des:[]},action)=>{
     switch(action.type){
         case STATUS:
             return {...state,des:state.des}
         default:
             return state;
     }
-} */
+}
